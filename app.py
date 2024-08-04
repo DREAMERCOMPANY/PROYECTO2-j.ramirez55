b567f8f9f5fa5f8c1b11a8dade10a755eba6bcb9
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2024-08-04 13:27:37 -0500

    Update Procfile and app configuration

--- app.py
+++ app.py
@@ -1,32 +1,6 @@
-# app.py
-from flask import Flask
-from config import Config
-from app.extensiones import db, migrate, login, bcrypt
+from app import create_app
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+app = create_app()
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-    login.init_app(app)
-    bcrypt.init_app(app)
-
-    login.login_view = 'auth.login'
-    login.login_message = 'Please log in to access this page.'
-
-    from app.models import User  
-
-    @login.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-
-    from app.auth import auth_bp
-    from app.routes import api as api_blueprint
-
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(api_blueprint, url_prefix='/api')
-
-    return app
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
